213d022df6d9e72c5277564dfdf7e6170fe8e184
cokoyoh <charlesokoyoh@gmail.com>
2020-08-06 12:38:18 +0300

    Add resolvers to resolve the query schemas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server');
+const sessions = require('./sessions.json');
 
 const typeDefs = gql`
 type Query {
@@ -18,10 +19,18 @@ type Session {
     level: String
 }`;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+    Query: {
+        sessions: () => {
+            return sessions;
+        }
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server
-    .listen({ port: process.env.PORT || 400 })
+    .listen({ port: process.env.PORT || 4000 })
     .then(({ url }) => {
         console.log(`graphql running at ${url}`);
     })
