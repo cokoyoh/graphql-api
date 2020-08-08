925f54afe316f25471ad06d2743fa8a3132ce9ca
cokoyoh <charlesokoyoh@gmail.com>
2020-08-08 09:46:31 +0300

    add multiple field filter and modularise code

--- datasources/sessions.js
+++ datasources/sessions.js
@@ -12,8 +12,8 @@ class SessionsAPI extends DataSource {
 
     }
 
-    getSessions() {
-        return sessions;
+    getSessions(args) {
+        return _.filter(sessions, args);
     }
 
     getSessionById(id) {
--- index.js
+++ index.js
@@ -1,35 +1,7 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const SessionsAPI = require('./datasources/sessions');
-
-const typeDefs = gql`
-type Query {
-    sessions: [Session],
-    sessionById(id: ID): Session
-}
-
-type Session {
-    id: ID!
-    title: String!
-    description: String
-    startAt: String
-    endsAt: String
-    room: String
-    day: String
-    format: String
-    track: String @deprecated(reason: "This will go away soon!")
-    level: String
-}`;
-
-const resolvers = {
-    Query: {
-        sessions: (parent, args, { dataSources }, info)  => {
-            return dataSources.sessionsAPI.getSessions();
-        },
-        sessionById: (parent, { id }, { dataSources }, info) => {
-            return dataSources.sessionsAPI.getSessionById(id);
-        }
-    }
-};
+const typeDefs = require('./schema.js');
+const resolvers = require('./resolvers.js');
 
 const dataSources = () => ({
     sessionsAPI: new SessionsAPI()
