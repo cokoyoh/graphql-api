01934c7856019e0ef08e9f0ff653b71322365c00
cokoyoh <charlesokoyoh@gmail.com>
2020-08-11 21:06:08 +0300

    Remove unncessaru thrown error

--- datasources/sessions.js
+++ datasources/sessions.js
@@ -18,7 +18,6 @@ class SessionsAPI extends DataSource {
 
     getSessionById(id) {
         const session = _.filter(sessions, { id: parseInt(id) });
-        throw new Error()
         return session[0]
     }
 
